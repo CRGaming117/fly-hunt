a13a58ddad839665511204d9949367c2213065a8
CRGaming117 <cantu7080@gmail.com>
2025-05-18 13:52:17 -0400

    Added Escape key bind to close game

--- hw04.py
+++ hw04.py
@@ -1,5 +1,3 @@
-# CS 172 Homework 4
-# Cristobal Cantu   cac554
 # MAIN CLASS
 import pygame
 import random
@@ -9,7 +7,7 @@ from text import Text
 
 pygame.init()
 surface = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Fly Hunt')
+pygame.display.set_caption("Fly Hunt")
 BLACK = (0, 0, 0)
 PINK = (255, 100, 200)
 GREEN = (0, 200, 0)
@@ -21,66 +19,101 @@ winNum = 0
 
 flies = []
 for i in range(20):
-    flies.append(Fly(random.randint(20,surface.get_width()-20),random.randint(20,surface.get_height()-20), 10, BLACK))
-    winNum+=1
+    flies.append(
+        Fly(
+            random.randint(20, surface.get_width() - 20),
+            random.randint(20, surface.get_height() - 20),
+            10,
+            BLACK,
+        )
+    )
+    winNum += 1
     flies[i].setSpeed(2)
 blueflies = []
 for i in range(5):
-    blueflies.append(Fly(random.randint(20,surface.get_width()-20),random.randint(20,surface.get_height()-20), 8, DARKBLUE))
-    winNum+=2
+    blueflies.append(
+        Fly(
+            random.randint(20, surface.get_width() - 20),
+            random.randint(20, surface.get_height() - 20),
+            8,
+            DARKBLUE,
+        )
+    )
+    winNum += 2
     blueflies[i].setSpeed(5)
 flies.extend(blueflies)
 ladybugs = []
 for i in range(3):
-    ladybugs.append(Fly(random.randint(20,surface.get_width()-20),random.randint(20,surface.get_height()-20), 12, RED))
+    ladybugs.append(
+        Fly(
+            random.randint(20, surface.get_width() - 20),
+            random.randint(20, surface.get_height() - 20),
+            12,
+            RED,
+        )
+    )
 flies.extend(ladybugs)
 
-frog1 = Frog(400, 300,30,PINK)
+frog1 = Frog(400, 300, 30, PINK)
 frog1.setVisible(False)
 scoreBoard = Text("Score: 0", 10, 10)
-winMsg = Text("You won!",0,0,GREEN,100)
-winMsg.setLoc((surface.get_width()//2)-(winMsg.get_rect().width//2), (surface.get_height()//2)-(winMsg.get_rect().height//2))
+winMsg = Text("You won!", 0, 0, GREEN, 100)
+winMsg.setLoc(
+    (surface.get_width() // 2) - (winMsg.get_rect().width // 2),
+    (surface.get_height() // 2) - (winMsg.get_rect().height // 2),
+)
 winMsg.setVisible(False)
-loseMsg = Text("You lost.",0,0,RED,100)
-loseMsg.setLoc((surface.get_width()//2)-(winMsg.get_rect().width//2), (surface.get_height()//2)-(winMsg.get_rect().height//2))
+loseMsg = Text("You lost.", 0, 0, RED, 100)
+loseMsg.setLoc(
+    (surface.get_width() // 2) - (winMsg.get_rect().width // 2),
+    (surface.get_height() // 2) - (winMsg.get_rect().height // 2),
+)
 loseMsg.setVisible(False)
 
 running = True
 fpsClock = pygame.time.Clock()
 while running:
-    surface.fill((0,0,255))
-    
+    surface.fill((0, 0, 255))
+
     for fly in flies:
         if winMsg.isVisible() or loseMsg.isVisible():
             fly.setVisible(False)
-        
+
         if (not loseMsg.isVisible()) or (not winMsg.isVisible()):
             fly.draw(surface)
-            if fly.getColor()==RED and fly.isVisible():
-                s=4
-                #pygame.draw.rect(surface, RED, fly.get_rect(),2)
-                pygame.draw.circle(surface,BLACK,fly.getLoc(),s)
-                if fly.getXSpeed()==0 or fly.getYSpeed()==0:
-                    d=6
-                    pygame.draw.circle(surface,BLACK,(fly.getX()-d,fly.getY()-d),s)
-                    pygame.draw.circle(surface,BLACK,(fly.getX()+d,fly.getY()-d),s)
-                    pygame.draw.circle(surface,BLACK,(fly.getX()-d,fly.getY()+d),s)
-                    pygame.draw.circle(surface,BLACK,(fly.getX()+d,fly.getY()+d),s)
+            if fly.getColor() == RED and fly.isVisible():
+                s = 4
+                # pygame.draw.rect(surface, RED, fly.get_rect(),2)
+                pygame.draw.circle(surface, BLACK, fly.getLoc(), s)
+                if fly.getXSpeed() == 0 or fly.getYSpeed() == 0:
+                    d = 6
+                    pygame.draw.circle(
+                        surface, BLACK, (fly.getX() - d, fly.getY() - d), s
+                    )
+                    pygame.draw.circle(
+                        surface, BLACK, (fly.getX() + d, fly.getY() - d), s
+                    )
+                    pygame.draw.circle(
+                        surface, BLACK, (fly.getX() - d, fly.getY() + d), s
+                    )
+                    pygame.draw.circle(
+                        surface, BLACK, (fly.getX() + d, fly.getY() + d), s
+                    )
                 else:
-                    d=9
-                    pygame.draw.circle(surface,BLACK,(fly.getX(),fly.getY()-d),s)
-                    pygame.draw.circle(surface,BLACK,(fly.getX()+d,fly.getY()),s)
-                    pygame.draw.circle(surface,BLACK,(fly.getX(),fly.getY()+d),s)
-                    pygame.draw.circle(surface,BLACK,(fly.getX()-d,fly.getY()),s)
-            
-        fly.move_better(fpsClock.get_time()>=35)
-        if fly.getColor()== DARKBLUE:
-            #print(fly.getLoc())
-            fly.escape(frog1,50)
-            
+                    d = 9
+                    pygame.draw.circle(surface, BLACK, (fly.getX(), fly.getY() - d), s)
+                    pygame.draw.circle(surface, BLACK, (fly.getX() + d, fly.getY()), s)
+                    pygame.draw.circle(surface, BLACK, (fly.getX(), fly.getY() + d), s)
+                    pygame.draw.circle(surface, BLACK, (fly.getX() - d, fly.getY()), s)
+
+        fly.move_better(fpsClock.get_time() >= 35)
+        if fly.getColor() == DARKBLUE:
+            # print(fly.getLoc())
+            fly.escape(frog1, 50)
+
         if fly.isVisible() and frog1.isVisible() and fly.isColliding(frog1):
             fly.setVisible(False)
-            print("fly caught")
+            # print("fly caught")
             if fly.getColor() == BLACK:
                 numEaten += 1
             elif fly.getColor() == DARKBLUE:
@@ -88,31 +121,66 @@ while running:
             elif fly.getColor() == RED:
                 loseMsg.setVisible(True)
             scoreBoard.setText("Score: " + str(numEaten))
-    
+
     frog1.draw(surface)
-    #pygame.draw.rect(surface, RED, frog1.get_rect(),2)
+    # pygame.draw.rect(surface, RED, frog1.get_rect(),2)
     frog1.follow_mouse()
 
     # scope
     if not frog1.isVisible():
-        pygame.draw.circle(surface, PINK, frog1.getLoc(),frog1.getSize()//2,2)
-        pygame.draw.line(surface,PINK,(frog1.getX(),frog1.getY()-frog1.getSize()),(frog1.getX(),frog1.getY()+frog1.getSize()),2)
-        pygame.draw.line(surface,PINK,(frog1.getX()-frog1.getSize(),frog1.getY()),(frog1.getX()+frog1.getSize(),frog1.getY()),2)
+        pygame.draw.circle(surface, PINK, frog1.getLoc(), frog1.getSize() // 2, 2)
+        pygame.draw.line(
+            surface,
+            PINK,
+            (frog1.getX(), frog1.getY() - frog1.getSize()),
+            (frog1.getX(), frog1.getY() + frog1.getSize()),
+            2,
+        )
+        pygame.draw.line(
+            surface,
+            PINK,
+            (frog1.getX() - frog1.getSize(), frog1.getY()),
+            (frog1.getX() + frog1.getSize(), frog1.getY()),
+            2,
+        )
     else:
-        pygame.draw.line(surface,PINK,(frog1.getX(),frog1.getY()),(surface.get_width()//2,surface.get_height()),frog1.getSize()-5)
-    #frog
+        pygame.draw.line(
+            surface,
+            PINK,
+            (frog1.getX(), frog1.getY()),
+            (surface.get_width() // 2, surface.get_height()),
+            frog1.getSize() - 5,
+        )
+    # frog
     povSize = 100
-    pygame.draw.circle(surface,GREEN,((surface.get_width()//2)-(povSize//2),surface.get_height()-int(povSize*.75)),int((povSize//2.5)))
-    pygame.draw.circle(surface,GREEN,((surface.get_width()//2)+(povSize//2),surface.get_height()-int(povSize*.75)),int((povSize//2.5)))
-    pygame.draw.circle(surface,GREEN,(surface.get_width()//2,surface.get_height()),povSize)
-    
+    pygame.draw.circle(
+        surface,
+        GREEN,
+        (
+            (surface.get_width() // 2) - (povSize // 2),
+            surface.get_height() - int(povSize * 0.75),
+        ),
+        int((povSize // 2.5)),
+    )
+    pygame.draw.circle(
+        surface,
+        GREEN,
+        (
+            (surface.get_width() // 2) + (povSize // 2),
+            surface.get_height() - int(povSize * 0.75),
+        ),
+        int((povSize // 2.5)),
+    )
+    pygame.draw.circle(
+        surface, GREEN, (surface.get_width() // 2, surface.get_height()), povSize
+    )
+
     scoreBoard.draw(surface)
-    
+
     winMsg.setVisible(numEaten == winNum)
     winMsg.draw(surface)
     loseMsg.draw(surface)
-    
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -120,7 +188,10 @@ while running:
             frog1.setVisible(True)
         elif event.type == pygame.MOUSEBUTTONUP:
             frog1.setVisible(False)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
     pygame.display.update()
-    fpsClock.tick(30) # 30 fps
+    fpsClock.tick(30)  # 30 fps
 exit()
